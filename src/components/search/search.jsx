60202a4226aaab01c864309b51d3d60c5a1479f1
Jamila <jamilaspears.dev@gmail.com>
2023-05-26 06:57:02 -0400

    Updating header responsiveness

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,12 +1,18 @@
 import './search.scss';
 import profile from '../../assets/images/Mohan-muruge.jpg';
 import searchPic from '../../assets/Icons/search.svg';
+import Button from '../button/button';
 
 function Search () {
     return (
         <div className="searchBar">
-            <input type='text' placeholder='Search'></input>
-            <img src={profile} alt= 'profile'></img>
+            <div className='searchBar__input'>
+                <input type='text' placeholder='Search'></input>
+            </div>
+            <div className='searchBar__image'>
+                <img src={profile} alt= 'profile'></img>
+            </div>
+            
         </div>
     )
 }
