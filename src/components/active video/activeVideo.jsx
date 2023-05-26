661e6de1e5be2934f013a4c26cacb71e29d2c44a
Jamila <jamilaspears.dev@gmail.com>
2023-05-26 05:18:06 -0400

    Added Active video, channel, likes, date, views, and description. Finished css for mobile sceen.

--- src/components/active video/activeVideo.jsx
+++ src/components/active video/activeVideo.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import videoDataJSON from '../../data/video-details.json';
 import './activeVideo.css';
+import views from '../../assets/Icons/views.svg';
+import likes from '../../assets/Icons/likes.svg';
 
 
 
@@ -10,11 +12,35 @@ import './activeVideo.css';
 function Active () {
     const [videoData, setVideoData] = useState(videoDataJSON);
     const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
+    const date = new Date (selectedVideo.timestamp).toLocaleDateString();
 
     return (
         <div className='activeVideoPlayer'>
-            <h1>{selectedVideo.title}</h1>
+            
             <img className = "activeVideo" src={selectedVideo.image} alt={selectedVideo.title} />
+            <p className='activeVideoPlayer__title'>{selectedVideo.title}</p>
+
+            <span className='activeVideo__divider'></span>
+            <div className='activeVideoData'>
+                <div className='activeVideoData__section1'>
+                    <p className='activeVideoData__section1--channel'>By {selectedVideo.channel}</p>
+                    <p className = 'activeVideoData__section1--date'>{date}</p>
+                </div>
+                <div className='activeVideoData__section2'>
+                    <div className='activeVideoData__viewsSection'>
+                        <img src={views} alt='views' />
+                        <p className='activeVideoData__viewsSection--count'>{selectedVideo.views}</p>
+                    </div>
+                    <div className='activeVideoData__likesSection'>
+                        <img src={likes} alt='likes' />
+                        <p className='activeVideoData__likesSection--likes'>{selectedVideo.likes}</p>
+                    </div>
+                </div>
+
+            </div>
+            <span className='activeVideo__divider'></span>
+
+            <p className='activeVideoData__description'>{selectedVideo.description}</p>
         </div>
 
 )}
